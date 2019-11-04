c6ed9a3c6c899e5793ec3724ccbf963bc99ec77c
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-11-04 16:40:34 -0500

    Created GET route to get account by specific ID'

--- accounts/accountsRouter.js
+++ accounts/accountsRouter.js
@@ -4,26 +4,36 @@ const db = require('../data/dbConfig.js');
 
 const router = express.Router();
 
-
+//GET accounts
 router.get('/', (req, res) => {
-    // list of posts
-    // SELECT * form Posts
-    //returns a promise
-    db.select ('*') .from ('accounts').then(accounts => {
-    res.status(200).json(accounts)
-    }).catch (error =>{
-        res.status(500).json({error: 'Fail to get'})
-    })
-    
-    });
 
-    router.post('/', (req, res) => {
-        db.insert(req.body, 'id') 
+    db.select('*').from('accounts').then(accounts => {
+        res.status(200).json(accounts)
+    }).catch(error => {
+        res.status(500).json({ error: 'Fail to get accounts' })
+    })
+
+});
+
+
+//POST account
+router.post('/', (req, res) => {
+    db.insert(req.body, 'id')
         .into('accounts')
         .then(ids => {
             res.status(200).json(ids)
-            }).catch (error =>{
-                res.status(500).json({error: 'Fail to get'})
-            })
-        });
+        }).catch(error => {
+            res.status(500).json({ error: 'Fail to post account' })
+        })
+});
+
+//GET account by id
+router.get('/:id', (req, res) => {
+    db.select('*').from('accounts').where('id', '=', req.params.id)
+        .then(account => {
+            res.status(200).json(account)
+        }).catch(error => {
+            res.status(500).json({ error: 'Failed to get account' })
+        })
+});
 module.exports = router;
