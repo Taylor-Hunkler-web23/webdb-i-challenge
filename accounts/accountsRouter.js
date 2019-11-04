422ffd5dc072b5d7ed86f8eab007079d19219167
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-11-04 16:27:48 -0500

    Created GET and POST route for accounts

--- accounts/accountsRouter.js
+++ accounts/accountsRouter.js
@@ -5,4 +5,25 @@ const db = require('../data/dbConfig.js');
 const router = express.Router();
 
 
+router.get('/', (req, res) => {
+    // list of posts
+    // SELECT * form Posts
+    //returns a promise
+    db.select ('*') .from ('accounts').then(accounts => {
+    res.status(200).json(accounts)
+    }).catch (error =>{
+        res.status(500).json({error: 'Fail to get'})
+    })
+    
+    });
+
+    router.post('/', (req, res) => {
+        db.insert(req.body, 'id') 
+        .into('accounts')
+        .then(ids => {
+            res.status(200).json(ids)
+            }).catch (error =>{
+                res.status(500).json({error: 'Fail to get'})
+            })
+        });
 module.exports = router;
